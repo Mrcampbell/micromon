e7708eb2d0b3ce7a7c3a277410e691230f1371cc
Mike Campbell <mike.campbell@getweave.com>
2019-06-22 15:56:24 -0600

    made protos

---
+++ gen.sh
@@ -0,0 +1,7 @@
+protoc --go_out=plugins=grpc:. *.proto
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --go_out=plugins=grpc:. \
+  **/*.proto
