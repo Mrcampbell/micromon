d44ca1aae54846cc8c458cb1c1111f99a6b71b28
Mike Campbell <mike.campbell@getweave.com>
2019-06-22 16:34:18 -0600

    added gateway

--- gen.sh
+++ gen.sh
@@ -5,3 +5,9 @@ protoc -I/usr/local/include -I. \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --go_out=plugins=grpc:. \
   **/*.proto
+
+protoc -I/usr/local/include -I. \
+  -I$GOPATH/src \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:. \
+  **/*.proto
