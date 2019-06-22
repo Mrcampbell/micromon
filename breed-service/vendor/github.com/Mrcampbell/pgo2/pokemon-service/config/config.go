2b9de1d4dc99431f471726f06b46cefee04c963b
Mike Campbell <mike.campbell@getweave.com>
2019-06-22 17:12:54 -0600

    added vendor

---
+++ breed-service/vendor/github.com/Mrcampbell/pgo2/pokemon-service/config/config.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	DBUser     = "postgres"
+	DBPassword = "password"
+	DBDatabase = "pokemon"
+	DBHost     = "db"
+	DBPort     = "5432"
+	GRPCPort   = ":9090"
+	HTTPPort   = ":8080"
+)
