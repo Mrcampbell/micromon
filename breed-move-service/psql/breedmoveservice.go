f8116182b137c9cecd17b7e6cfcf631f61a12f30
Mike Campbell <mike.campbell@getweave.com>
2019-06-26 23:26:51 -0600

    added db ref to breed-move-service

--- breed-move-service/psql/breedmoveservice.go
+++ breed-move-service/psql/breedmoveservice.go
@@ -1,12 +1,35 @@
 package psql
 
-type BreedMoveService struct{}
+import (
+	"fmt"
 
-func NewBreedMoveService() (*BreedMoveService, error) {
-	return &BreedMoveService{}, nil
+	"github.com/Mrcampbell/pgo2/breed-move-service/config"
+	"github.com/go-pg/pg"
+)
+
+type BreedMoveService struct {
+	DB *pg.DB
 }
 
+func NewBreedMoveService() (*BreedMoveService, error) {
+	db := pg.Connect(&pg.Options{
+		Addr:     config.DBHost + ":" + config.DBUser,
+		Database: config.DBDatabase,
+		User:     config.DBUser,
+		Password: config.DBPassword,
+		OnConnect: func(conn *pg.Conn) error {
+			fmt.Println("Connected to DB")
+			return nil
+		},
+	})
 
+	// no need to initialize or create schema because it should exist.
+	// It's a huge process right now
+
+	return &BreedMoveService{
+		DB: db,
+	}, nil
+}
 
 // GetAllPreviousLevelUpLearnableMoveIDsForBreed gets ALL the possible moves for a pokemon to have learned in the wild.
 //  Useful for when generating wild pokemon and you need a random moveset.
