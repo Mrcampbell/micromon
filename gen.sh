1c699e1a85f1e8b5776cdad170efe40fbb4cfc16
Mike Campbell <mike.campbell@getweave.com>
2019-07-01 14:59:44 -0600

    updated script and made it so it runs from main dir and updates all the projects

--- gen.sh
+++ gen.sh
@@ -17,3 +17,19 @@ protoc-go-inject-tag -input=./pokemon/pokemon.pb.go
 gv update github.com/Mrcampbell/pgo2/protorepo/pokemon
 
 protoc-go-inject-tag -input=./pokemon.pb.go  
+(cd breed-move-service && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
+(cd breed-service && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
+(cd gateway-api && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
+(cd move-service && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
+(cd pokemon-service && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
+
+NLU=nsqlookupd
+NLU_C="$NLU | tee 1.log | sed -e 's/^/[nsqlookupd] /'"
+
+NSQD="nsqd --lookupd-tcp-address=127.0.0.1:4160 -broadcast-address=127.0.0.1"
+NSQD_C="$NSQD | tee 2.log | sed -e 's/^/[nsqd] /'"
+
+NSQADMIN="nsqadmin --lookupd-http-address=127.0.0.1:4161"
+NSQADMIN_C="$NSQADMIN | tee 3.log | sed -e 's/^/[nsqadmin] /'"
+
+$NLU_C & $NSQD_C & $NSQADMIN_C
