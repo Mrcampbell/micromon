fc3422252b8e4ae0242a2723ef56959f9892a8ea
Mike Campbell <mike.campbell@getweave.com>
2019-07-01 15:00:06 -0600

    added change dir

--- gen.sh
+++ gen.sh
@@ -1,5 +1,10 @@
 protoc --go_out=plugins=grpc:. *.proto
 
+#############################
+
+
+cd protorepo 
+
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
@@ -14,7 +19,7 @@ protoc -I/usr/local/include -I. \
 
 protoc-go-inject-tag -input=./pokemon/pokemon.pb.go  
 
-gv update github.com/Mrcampbell/pgo2/protorepo/pokemon
+cd ../
 
 protoc-go-inject-tag -input=./pokemon.pb.go  
 (cd breed-move-service && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
@@ -23,13 +28,8 @@ protoc-go-inject-tag -input=./pokemon.pb.go
 (cd move-service && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
 (cd pokemon-service && gv update github.com/Mrcampbell/pgo2/protorepo/pokemon)
 
-NLU=nsqlookupd
-NLU_C="$NLU | tee 1.log | sed -e 's/^/[nsqlookupd] /'"
+################################
 
-NSQD="nsqd --lookupd-tcp-address=127.0.0.1:4160 -broadcast-address=127.0.0.1"
-NSQD_C="$NSQD | tee 2.log | sed -e 's/^/[nsqd] /'"
 
-NSQADMIN="nsqadmin --lookupd-http-address=127.0.0.1:4161"
-NSQADMIN_C="$NSQADMIN | tee 3.log | sed -e 's/^/[nsqadmin] /'"
 
-$NLU_C & $NSQD_C & $NSQADMIN_C
+gv update github.com/Mrcampbell/pgo2/protorepo/pokemon
