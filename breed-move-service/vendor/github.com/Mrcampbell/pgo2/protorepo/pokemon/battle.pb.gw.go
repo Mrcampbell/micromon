07f8fa64d54a6e744a401cecb1bc502a936fc9b1
Mike Campbell <mike.campbell@getweave.com>
2019-07-02 15:28:54 -0600

    added missing body tag

--- breed-move-service/vendor/github.com/Mrcampbell/pgo2/protorepo/pokemon/battle.pb.gw.go
+++ breed-move-service/vendor/github.com/Mrcampbell/pgo2/protorepo/pokemon/battle.pb.gw.go
@@ -72,13 +72,17 @@ func request_BattleService_GetBattle_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
-var (
-	filter_BattleService_UseAttack_0 = &utilities.DoubleArray{Encoding: map[string]int{"battle_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
-)
-
 func request_BattleService_UseAttack_0(ctx context.Context, marshaler runtime.Marshaler, client BattleServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq UseAttackRequest
 	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	var (
 		val string
@@ -96,10 +100,6 @@ func request_BattleService_UseAttack_0(ctx context.Context, marshaler runtime.Ma
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "battle_id", err)
-	}
-
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_BattleService_UseAttack_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.UseAttack(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
