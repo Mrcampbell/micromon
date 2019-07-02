320ab98d5f33532b9130f639b0b09c0da7a4df43
Mike Campbell <mike.campbell@getweave.com>
2019-07-02 14:29:37 -0600

    added uuid

---
+++ battle-service/vendor/github.com/Mrcampbell/pgo2/shared-library/uuid/uuid.go
@@ -0,0 +1,23 @@
+package uuid
+
+import (
+	"fmt"
+
+	guuid "github.com/google/uuid"
+)
+
+func PrefixedUUID(prefix string) string {
+	var cleanedPrefix string
+
+	if len(prefix) > 2 {
+		fmt.Println("Prefix is Too Long.  It's been Trimmed to 2 Characters")
+		cleanedPrefix = prefix[0:2] + "_"
+	} else if len(prefix) == 0 {
+		fmt.Println("Prefix not provided.  Just so you know")
+	} else {
+		cleanedPrefix = prefix + "_"
+	}
+
+	id, _ := guuid.NewRandom()
+	return fmt.Sprintf("%s%s", cleanedPrefix, id)
+}
